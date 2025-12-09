9b68311b88c9d320b690c7685b6c1fa20c0f6c77
Pavel Romanishkin <me@twttr.io>
2025-12-09 12:39:51 +0100

    switch to working hotkey approach

--- Utilities/HotkeyManager.swift
+++ Utilities/HotkeyManager.swift
@@ -1,31 +1,67 @@
+import Carbon
 import Cocoa
 
 @MainActor
 class HotkeyManager {
   static let shared = HotkeyManager()
 
-  private var keyMonitor = KeyMonitor()
+  private var eventHandler: EventHandlerRef?
+  private var hotKeyRef: EventHotKeyRef?
+
   var onHotkeyPressed: (() -> Void)?
 
   private init() {}
 
   func registerHotkey() {
-    let options: NSDictionary = [
-      kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true
-    ]
-    _ = AXIsProcessTrustedWithOptions(options as CFDictionary)
+    guard eventHandler == nil else { return }
 
-    keyMonitor.start(
-      keyCode: 11,
-      modifiers: [.command, .shift],
-      scope: .global
-    ) { [weak self] in
-      self?.onHotkeyPressed?()
+    var eventType = EventTypeSpec(
+      eventClass: OSType(kEventClassKeyboard),
+      eventKind: UInt32(kEventHotKeyPressed)
+    )
+
+    let status = InstallEventHandler(
+      GetEventDispatcherTarget(),
+      { (_, event, _) -> OSStatus in
+        _ = HotkeyManager.shared.handleHotKeyEvent(event)
+        return noErr
+      },
+      1,
+      &eventType,
+      nil,
+      &eventHandler
+    )
+
+    guard status == noErr else { return }
+
+    let hotKeyID = EventHotKeyID(signature: OSType(0x424C5259), id: 1)
+
+    RegisterEventHotKey(
+      UInt32(kVK_ANSI_B),
+      UInt32(cmdKey | shiftKey),
+      hotKeyID,
+      GetEventDispatcherTarget(),
+      0,
+      &hotKeyRef
+    )
+  }
+
+  private nonisolated func handleHotKeyEvent(_ event: EventRef?) -> OSStatus {
+    Task { @MainActor in
+      self.onHotkeyPressed?()
     }
+    return noErr
   }
 
   func unregisterHotkey() {
-    keyMonitor.stop()
+    if let hotKeyRef = hotKeyRef {
+      UnregisterEventHotKey(hotKeyRef)
+      self.hotKeyRef = nil
+    }
+    if let eventHandler = eventHandler {
+      RemoveEventHandler(eventHandler)
+      self.eventHandler = nil
+    }
   }
 
   func cleanup() {
