1bcf01197601c4f90ad1fb8ec306790cc1d750c1
Pavel Romanishkin <me@twttr.io>
2025-12-09 15:31:26 +0100

    rename menu item, get rid of scope, add entitlement, enable sandbox

--- Utilities/KeyMonitor.swift
+++ Utilities/KeyMonitor.swift
@@ -1,43 +1,23 @@
 import Cocoa
 
 class KeyMonitor {
-  enum Scope {
-    case local
-    case global
-  }
-  
   private var monitor: Any?
-  
+
   func start(
     keyCode: UInt16,
     modifiers: NSEvent.ModifierFlags = [],
-    scope: Scope = .local,
     handler: @escaping () -> Void
   ) {
     stop()
-    
-    let matcher: (NSEvent) -> Bool = { event in
-      guard event.keyCode == keyCode else { return false }
-      if modifiers.isEmpty { return true }
-      let eventMods = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
-      return eventMods == modifiers
-    }
-    
-    switch scope {
-    case .local:
-      monitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
-        if matcher(event) {
-          handler()
-          return nil
-        }
-        return event
+
+    monitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+      guard event.keyCode == keyCode else { return event }
+      if !modifiers.isEmpty {
+        let eventMods = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+        guard eventMods == modifiers else { return event }
       }
-    case .global:
-      monitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
-        if matcher(event) {
-          handler()
-        }
-      }
+      handler()
+      return nil
     }
   }
   
